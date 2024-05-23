8d2d635852b418bf53c55de4896ab6c4bfa7df72
JFlandre <chunyuen0812@gmail.com>
2024-05-23 23:38:37 +0800

    add Redis Config

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,27 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("create redis template object");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        // set connection factory
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // set key serializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+
+
+    }
+}
